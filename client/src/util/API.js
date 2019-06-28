8b349eecc78e370d8cb3f5641a882a57c3ab0c23
Lorenzo <lgdandrea4@gmail.com>
2019-06-28 11:48:24 -0500

    updated

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -2,5 +2,15 @@ import axios from 'axios';
 
 export default {
     getAllEvents: () => axios.get('/api/allevents')
+        .then( response => response.data ),
+    getMyEvents: () => axios.get('/api/myevents')
+        .then( response => response.data),
+    createNewEvent : () => axios.post('/api/newevent')
+        .then( response => response.data ),
+    createNewUser : () => axios.post('/api/newuser')
+        .then( response => response.data ),
+    updateEvent: () => axios.put('/api/update')
+        .then( response => response.data ),
+    removeEvent: () => axios.delete('/api/remove')
         .then( response => response.data )
 }
