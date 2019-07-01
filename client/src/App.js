5a8b410fb34e86e82cd638eb26247d7e447e94ef
Lorenzo <lgdandrea4@gmail.com>
2019-07-01 15:26:44 -0500

    got the front page configured and ordered and added some CSS to the page

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,23 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import "./App.css";
 import LandingPage from './pages/LandingPage';
 import RegisterPage from './components/SignUpForm';
-import WeatherWidget from './components/weatherWidget'
+import WeatherWidget from './components/weatherWidget';
+import MainPugs from './pages/MainPugs';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="main">
+        <div className="main">
           <BrowserRouter>
             <Route exact path="/" component={() => <LandingPage />} />
             <Route exact path="/weather" component={() => <WeatherWidget />} />
             <Route exact path="/register" component={() => <RegisterPage className="text-left mx-auto"/>}/>
-            <Route exact path='/home' component={() => <div>Main Pugs Page</div>}></Route>
-            <Route exact path='/mypugs' component={() => <div>My Pugs Page</div>}></Route>
-            <Route exact path='/create' component={() => <div>Create a pug here</div>}></Route>
+            <Route exact path="/home" component={() => <MainPugs />}></Route> 
+            <Route exact path="/mypugs" component={() => <div>My Pugs Page</div>}></Route>
+            <Route exact path="/create" component={() => <div>Create a pug here</div>}></Route>
           </BrowserRouter>
-          </div>
+        </div>
       </div>
     );
   }
