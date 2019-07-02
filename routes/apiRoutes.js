397a5d6dd97129d1e8bbdcc1efc701ff123f77b9
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-07-02 10:37:47 -0500

    routes setup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 var db = require("../models");
 var router = require('express').Router();
 
-router.route('/allevents')
-    .get((req,res,err) => {
-        db.Events.findAll({})
-        .then(function(dbevents) {
-            res.json(dbevents);
-            console.log("IT was sent")
-        });
+router.get("/allevents", function(req,res) {
+     //console.log("hello")
+    // console.log(db.Events);
+    db.Events.findAll({})
+    .then(function(dbevents) {
+        res.json(dbevents);
     });
+});
 
 router.route('/myevents')
     .get((req,res,err) => {
@@ -40,18 +39,8 @@ router.route('/user')
     .get((req,res,err) => {
         res.json();
     });
-=======
-var router = require("express").Router();
-var db = require("../models");
 
-router.get("/allevents", function(req,res) {
-    // console.log("hello")
-    // console.log(db.Events);
-    db.Events.findAll({})
-    .then(function(dbevents) {
-        res.json(dbevents);
-    });
-});
->>>>>>> 88560a8524acb53391b24eac1b728bd20c027692
+
+
 
 module.exports = router;
