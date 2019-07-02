08107531ce5cb8d3bd721f2fa8021396fcc0de50
Ellen Buelow <buelowek1225@gmail.com>
2019-07-02 11:06:19 -0500

    added event name column and updated dummy data

--- models/dummydata.sql
+++ models/dummydata.sql
@@ -14,14 +14,14 @@ INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Bas
 INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Cards', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- create events
-INSERT INTO events (start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('2019-08-25', '2019-08-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Volleyball', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', '1');
-INSERT INTO events (start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('2019-07-01', '2019-07-01', '08:00:00', 'Minneapolis', 'MN', 53061, 'Settlers of Catan', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '2', '2');
-INSERT INTO events (start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Euchre', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
-INSERT INTO events (start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Cribbage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
+INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
+values ('Sassy-Servers', '2019-08-25', '2019-08-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Volleyball', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', '1');
+INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
+values ('Catan Masters', '2019-07-01', '2019-07-01', '08:00:00', 'Minneapolis', 'MN', 53061, 'Settlers of Catan', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '2', '2');
+INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
+values ('Euchre-Yoopers', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Euchre', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
+INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
+values ('Criptic Cribbage', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Cribbage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
 
 
 -- create particpant
