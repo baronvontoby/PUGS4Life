4cb9da18c30ea2ba5f9a713c8f0ae85e4b3da261
Ellen Buelow <buelowek1225@gmail.com>
2019-07-02 13:50:08 -0500

    updating routes

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ var syncOptions = { force: true };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+  // this controls the reset of our mysql database
+  syncOptions.force = false;
 }
 
 // Starting the server, syncing our models ------------------------------------/
