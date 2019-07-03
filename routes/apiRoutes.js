a78c441982933ba86d169ec80fd5e99a7673be21
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-07-03 09:55:22 -0500

    commiting changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,6 @@
 var db = require("../models");
 var router = require('express').Router();
 
-<<<<<<< HEAD
-// call all events
-router.get("/api/allevents", function(req,res) {
-     //console.log("hello")
-    // console.log(db.Events);
-    db.Events.findAll({})
-    .then(function(dbevents) {
-        res.json(dbevents);
-    });
-=======
 // call all events WORKING
 router.get("/allevents", function(req,res) {
   //console.log("hello")
@@ -19,7 +9,6 @@ router.get("/allevents", function(req,res) {
   .then(function(dbevents) {
       res.json(dbevents);
   });
->>>>>>> ad396c67616af73a2eb0c9234a230ee0c0527928
 });
 
 // add new event WORKING (in postman a json of the add event will be returned)
@@ -43,20 +32,6 @@ router.route('/newuser')
       .catch(err => res.json(500, err))
   });
 
-<<<<<<< HEAD
-
-router.put('/update/:id', function(req, res){
-    db.Events.update(
-        req.body,
-        {
-            where: {
-                id: req.params.id
-            }
-        })
-    .then(function(dbevents){
-        res.json(dbevents);
-    });
-=======
 // update event information WORKING
 router.put('/update/:id', function(req, res){
   db.Events.update(
@@ -69,22 +44,9 @@ router.put('/update/:id', function(req, res){
     .then( (dbevents) => {
       // this will return the number of rows updated in database
       res.json(dbevents);
->>>>>>> ad396c67616af73a2eb0c9234a230ee0c0527928
     });
 });
 
-<<<<<<< HEAD
-router.route('/api/remove/:id')
-    .delete((req,res,err) => {
-     db.Events.destroy({
-         where:{
-             id: req.params.id}
-         })
-        .then(function(dbdelete) {
-            res.json(dbdelete)
-        });
-    });
-=======
 
 // Deletes event by id and returns json of number of rows affected. 
 router.delete('/remove/:id', function(req, res) {
@@ -97,7 +59,6 @@ router.delete('/remove/:id', function(req, res) {
     res.json(dbdelete);
   });
 });
->>>>>>> ad396c67616af73a2eb0c9234a230ee0c0527928
 
 router.route('/api/user')
     .get((req,res,err) => {
