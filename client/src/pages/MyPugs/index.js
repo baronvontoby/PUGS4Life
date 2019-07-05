740d018f676053ceb6715fcbc0f2126295d2a3af
Jeff Samuels <jeffsamuelsmadison@gmail.com>
2019-07-05 13:39:01 -0500

    My Pugs and Joined Pugs on user page

--- client/src/pages/MyPugs/index.js
+++ client/src/pages/MyPugs/index.js
@@ -12,43 +12,47 @@ class MyPugs extends React.Component {
             <div>
                 <NavBar />
                 <MDBContainer>
-                    <row></row>
 
-                    <MDBRow>
-                        <MDBCol className="justify-content-center">
+                    <MDBRow class="text-center">
+                        <MDBCol></MDBCol>
+
+                        <MDBCol class="text-center" className="justify-content-center">
                             <img src="https://grfpublishers.com/assets/vendor/img/board.png" alt="avatar" className="img-thumbnail" />
-                        </MDBCol>
-                        <MDBCol className="justify-content-center">
                             <h1><strong>BARON VON TOBY</strong></h1>
                             <h2><small className="text-muted">Pro Pugger since 2019</small></h2>
                         </MDBCol>
+                        <MDBCol className="justify-content-center">
+
+                        </MDBCol>
                     </MDBRow>
 
-                
+                    <MDBRow>
+
+                    </MDBRow>
 
                     <MDBRow>
 
                         <MDBCol class="col-sm" className="justify-content-center">
-                        <MDBIcon icon="dog" size="5x" />
-                        <h1><strong>PUP</strong></h1>
+                            <MDBIcon icon="dog" size="5x" />
+                            <h1><strong>PUP</strong></h1>
                         </MDBCol>
-
-                        <MDBCol  class="col-sm" className="justify-content-center">
-                        <div className="my-5">
-                            <label htmlFor="customRange1"></label>
-                            <input type="range" className="custom-range" id="customRange1" />
-                            <h2 className="justify-content-center"><strong>SKILL LEVEL</strong></h2>
-                        </div>
-                        </MDBCol>
-
+                        <MDBCol></MDBCol>
                         <MDBCol class="col-sm" className="justify-content-center">
-                        <MDBIcon icon="fire-alt" size="5x" />
-                        <h1><strong>PRO</strong></h1>
+                            <div className="my-5">
+                                <label htmlFor="customRange1"></label>
+                                <input type="range" className="custom-range" id="customRange1" />
+                                <h2 className="justify-content-center"><strong>SKILL LEVEL</strong></h2>
+                            </div>
+                        </MDBCol>
+                        <MDBCol></MDBCol>
+                        <MDBCol class="col-sm" className="justify-content-center">
+                            <MDBIcon icon="fire-alt" size="5x" />
+                            <h1><strong>PRO</strong></h1>
                         </MDBCol>
 
-                        
-                       
-                        
+
+
+
 
                     </MDBRow>
 
@@ -57,7 +61,28 @@ class MyPugs extends React.Component {
 
                     </MDBRow>
 
+                    <MDBRow className="pugsIveMade">
 
+                        <MDBCol>
+
+                            <h1><strong>PUGS I've made here...</strong></h1>
+                            <hr></hr>
+                            <hr></hr>
+                        </MDBCol>
+
+                    </MDBRow>
+
+                    <MDBRow className="pugsIveJoined">
+
+                    <MDBCol>
+
+                    <h1><strong>PUGS I've joined here...</strong></h1>
+                    <hr></hr>
+                    <hr></hr>
+
+                    </MDBCol>
+                       
+                    </MDBRow>
 
                 </MDBContainer>
             </div >
