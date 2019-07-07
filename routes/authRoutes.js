fd2b45eff3a2d340c2f9dfc0b2a3f5404332390f
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-07-07 18:38:35 -0500

    fixing merge issues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,9 @@ var jwt = require('jsonwebtoken');
 
 router.route('/login')
     .post((req, res, err) => {
-        //console.log(req.body);
-        //console.log("username is" , req.body.username);
+        
         db.User.findOne({ where: {email: req.body.username} })
-            //.then(dbUser => {console.log(dbUser); return dbUser})
             .then(dbUser => dbUser ? dbUser : Promise.reject("no such user found") ) //check if the user exists
-            //.then(dbUser => {console.log( "the password1 is: ", dbUser.password , "the request password is " , req.body.password); return dbUser })
             .then(dbUser => {
                 if (req.body.password === dbUser.password ) {
                     return dbUser;
@@ -20,20 +17,17 @@ router.route('/login')
                 else {
                     Promise.reject('Incorrect Password! the user is: ' , dbUser)
                 }
-            })   // console.log( "the password is: ", dbUser.password , "the request password is " , req.body.password)
-            //.then(dbUser => {console.log(req.body.password === dbUser.password ); console.log("Is Undefined", dbUser); return dbUser })
+            })   
             .then(dbUser => {
-                //console.log("It is validated");
-                //let user = {};
+            
                 const token = jwt.sign(
                     {
                     data: [ { username: dbUser.username, email: dbUser.email, ph_num:dbUser.phone_num, zipcode: dbUser.zipcode } ]
                     }, SECRET_KEY) ; //created the key
-                    //console.log(dbUser);
+        
                     res.json({sucess: true, token: token, user: { id: dbUser.id , username: dbUser.username, email: dbUser.email, ph_num:dbUser.phone_num, zipcode: dbUser.zipcode } } ) //send back the token            
                 })
             .catch( err => res.json(401, err)); 
-        //res.json(req.body.Authorization);
     });
 
     router.route('/signUp') 
@@ -49,14 +43,15 @@ router.route('/login')
           city: req.body.phone_num,
           state: req.body.city,
           zipcode: req.body.state
-      }).then( newUser => 
+      }).then( newUser => {
             const token = jwt.sign(
             {
             data: [ { username: dbUser.username, email: dbUser.email, ph_num:dbUser.phone_num, zipcode: dbUser.zipcode } ]
-            }, SECRET_KEY) ; //created the key
-            //console.log(dbUser);
+            }, SECRET_KEY);
+  
             res.json({sucess: true, token: token, user: { id: dbUser.id , username: dbUser.username, email: dbUser.email, ph_num:dbUser.phone_num, zipcode: dbUser.zipcode } } ) //send back the token            
-    }); 
+    });
+}) 
  
   
 module.exports = router;
