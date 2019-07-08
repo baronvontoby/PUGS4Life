78c4278bd73cfe80dd361abb2193ca0f52cee947
Ellen Buelow <buelowek1225@gmail.com>
2019-07-08 13:53:28 -0500

    preparing to pull Deepa's revisions

--- models/config/jwt.js
+++ models/config/jwt.js
@@ -1,7 +1,7 @@
 const JWT_SECRET_KEY = 'b0TX6u4GeLJ0EUHcR9iMxH02HLEacArt'; //hide the key in the config vars
 
 var db = require("../");
-var db = require("../../models");
+// var db = require("../../models");
 
 
 //use dummy user until we are ready with real users database
