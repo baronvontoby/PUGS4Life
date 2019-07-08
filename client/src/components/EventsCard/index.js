bad3977d2c2b8c80f6d73cea9d4c0fb76e90635f
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-07-08 13:56:49 -0500

    event card styling

--- client/src/components/EventsCard/index.js
+++ client/src/components/EventsCard/index.js
@@ -1,32 +1,87 @@
-import React from 'react';
+import React from "react";
 // import {Card, Button, CardHeader, CardFooter, CardBody, CardTitle, CardText} from 'reactstrap';
 // import moment from 'moment';
-import {MDBRow, MDBCard, MDBCardBody,MDBCardText, MDBCardHeader, MDBCol, MDBCardImage, MDBBtn, MDBCardTitle, MDBCardFooter } from 'mdbreact';
+import {
+  MDBRow,
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBCardHeader,
+  MDBCol,
+  MDBCardImage,
+  MDBBtn,
+  MDBCardTitle,
+  MDBCardFooter
+} from "mdbreact";
 
-import './index.css';
-import avatar from './avatar.png'
-
-
-
+import "./index.css";
+import avatar from "./avatar.png";
 
 const EventsCard = props => (
+  <MDBCard className="my-3 mx-auto p-2 w-75 z-depth-2">
+    <MDBRow className="mx-auto">
+      <MDBCol md="3 align-self-center">
+        <MDBCardImage
+          src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F3%2F3b%2FSpongeBob_SquarePants_character.svg%2F1200px-SpongeBob_SquarePants_character.svg.png&f=1"
+          className="img-thumbnail w-100 rounded-circle mx-auto img-responsive p-0 waves"
+        />
+      </MDBCol>
+      <MDBCol md="7" style={{ backgroundColor: "white" }}>
+        <MDBRow className="align-items-center flex-wrap pt-3">
+          <MDBCol md="8 h3">{props.events.event_name}</MDBCol>
+          <MDBCol md="4 h5">{props.events.event_time}</MDBCol>
+        </MDBRow>
+        <MDBRow>
+          <MDBCol md="12">
+            <h6>
+              {props.events.event_city}, {props.events.event_state}
+            </h6>
+          </MDBCol>
+          <MDBCol md="12 mx-auto">
+            <p className=" border-bottom p-1 text-left my-2">
+              {props.events.description}
+            </p>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="mx-auto justify-content-center">
+          {/* <MDBCol md="2 mx-auto">
+            <MDBCardImage src={avatar} className="img-fluid img avatar" />
+          </MDBCol> */}
+          <MDBCol md="8 text-left px-0 py-2">
+            <p className="font-italic m-0 p-0 text-muted"><i className="text-center mx-auto left fas fa-user-circle fa-lg"></i> Created By: Liz Lemon</p>
+          </MDBCol>
+          <MDBCol md="4 align-self-center">
+            <p className="font-italic m-0 p-0 text-muted font-smaller "><i class="fas fa-circle green-text text-sm-right fa-sm"></i> 10 Friends</p>
+          </MDBCol>
+        </MDBRow>
+      </MDBCol>
+      <MDBCol md="2 mx-auto text-center">
+      <MDBRow className="align-items-end">
+        <MDBCol md="12">
+            <MDBBtn
+            className="btn btn-default Ripple-parent  mx-auto rounded py-2 px-4"
+            onClick={() =>
+                props.joinEvent(localStorage.getItem("newUser.id"), props.events.id)
+            }
+            >
+            Join <i className="mr-2 mx-auto fal fa-calendar-plus fa-lg" />
+            </MDBBtn>
+        </MDBCol>
 
-            <MDBCard className="my-3 mx-auto px-2">
-                <MDBRow>
-                    <MDBCol md="3">
-                    <MDBCardImage src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F3%2F3b%2FSpongeBob_SquarePants_character.svg%2F1200px-SpongeBob_SquarePants_character.svg.png&f=1" className="img-thumbnail img-fluid w-75 rounded-circle mx-auto">
-                        
-                        </MDBCardImage>
+      </MDBRow>
+      </MDBCol>
+    </MDBRow>
+    {/* <MDBRow className="mx-auto">
+                <MDBCol md="3">
+
+                </MDBCol>
+                    <MDBCol md="6">
+                    <MDBCardImage src="avatar"></MDBCardImage>
+
                     </MDBCol>
-                    <MDBCol md="7" style={{backgroundColor:"blue"}}>
-BEEP
-                    </MDBCol>
-                    <MDBCol md="2" style={{backgroundColor:"red"}}>
-BOOP
-                    </MDBCol>
-                </MDBRow>
-            </MDBCard>
-          /*  <MDBCard className="card mx-auto row my-3 justify-content-center px-2">
+                </MDBRow> */}
+  </MDBCard>
+  /*  <MDBCard className="card mx-auto row my-3 justify-content-center px-2">
                 <MDBCol className="col-md-3">
                     <MDBCardImage src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F3%2F3b%2FSpongeBob_SquarePants_character.svg%2F1200px-SpongeBob_SquarePants_character.svg.png&f=1" className="img-thumbnail">
                         
@@ -65,9 +120,6 @@ BOOP
             </MDBCard> 
             
             */
-
-     
-)
-
+);
 
 export default EventsCard;
