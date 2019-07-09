ac18271296afbebbf89d14f9c9a1dbedf76b2913
Ellen Buelow <buelowek1225@gmail.com>
2019-07-08 22:06:42 -0500

    pushing change to seed script

--- models/dummydata.sql
+++ models/dummydata.sql
@@ -7,12 +7,6 @@ INSERT INTO users (username, password, email, name, image_link, phone_num, city,
 INSERT INTO users (username, password, email, name, image_link, phone_num, city, state, zipcode, active, createdAt, updatedAt) values ('pbuelow', '123456', 'email3@gmail.com', 'phillip', NULL, '7155675301', 'Minneapolis', 'MN', '55111', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO users (username, password, email, name, image_link, phone_num, city, state, zipcode, active, createdAt, updatedAt) values ('sthong', '123456', 'email4@gmail.com', 'sabrina', NULL, '7155675302', 'Minneapolis', 'MN', '55111', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
--- create game category
-INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Outdoor', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Indoor', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Basketball', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- INSERT INTO gamecategories (name, is_outdoor, createdAt, updatedAt) values ('Cards', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
 -- create events
 INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
 values ('Sassy-Servers', '2019-08-25', '2019-08-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Volleyball', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', '1');
