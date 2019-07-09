cc61a0eab73816e1c52498c81ff88d8adf83d95a
Ellen Buelow <buelowek1225@gmail.com>
2019-07-08 22:20:57 -0500

    fixing stuff

--- models/dummydata.sql
+++ models/dummydata.sql
@@ -13,9 +13,9 @@ values ('Sassy-Servers', '2019-08-25', '2019-08-25', '08:00:00', 'Minneapolis', 
 INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
 values ('Catan Masters', '2019-07-01', '2019-07-01', '08:00:00', 'Minneapolis', 'MN', 53061, 'Settlers of Catan', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '2', '2');
 INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('Euchre-Yoopers', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Euchre', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
+values ('Euchre-Yoopers', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Euchre', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '2', '3');
 INSERT INTO events (event_name, start_date, end_date, event_time, event_city, event_state, event_zipcode, description, createdAt, updatedAt, GameCategoryId, UserId) 
-values ('Criptic Cribbage', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Cribbage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '4', '3');
+values ('Criptic Cribbage', '2019-07-25', '2019-07-25', '08:00:00', 'Minneapolis', 'MN', 53061, 'Cribbage', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '2', '3');
 
 
 -- create particpant
